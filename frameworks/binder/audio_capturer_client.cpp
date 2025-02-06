de01814f75806bc98328a9f4d3ddb3ccebe34c2a
jiao <jiaoruiting@huawei.com>
2025-02-06 20:01:13 +0800

    变量类型修改
    Signed-off-by: jiao <jiaoruiting@huawei.com>

--- frameworks/binder/audio_capturer_client.cpp
+++ frameworks/binder/audio_capturer_client.cpp
@@ -324,10 +324,10 @@ bool AudioCapturer::AudioCapturerClient::Start()
         return false;
     }
     
-    int32_t ret = proxy_->Invoke(proxy_, AUD_CAP_FUNC_START, &io, &para, ProxyCallbackFunc);
+    bool ret = proxy_->Invoke(proxy_, AUD_CAP_FUNC_START, &io, &para, ProxyCallbackFunc);
     if (ret) {
         MEDIA_ERR_LOG("Start failed, ret=%d", ret);
-        return ret;
+        return true;
     }
 
     return para.ret;
